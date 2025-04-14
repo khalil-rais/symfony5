59c5565de98234360360fcd91ce245450fc8c7fd
Khalil Raies <khalil.raies@check24.de>
2025-04-14 15:07:19 +0200

    Course 1 - Chapter 16: Example of logger service

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -6,14 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
     /**
      * @Route("/", name="app_homepage")
      */
-    public function homepage(){
-        return $this->render('question/homepage.html.twig');
+    public function homepage(Environment $twigEnvironment){
+        // fun example of using the Twig service directly
+        $html = $twigEnvironment->render('question/homepage.html.twig');
+        return new Response($html);
+        //return $this->render('question/homepage.html.twig');
     }
 
     /**
