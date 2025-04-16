18fa374ad000096e1502b36c328e7e2da2ea3285
Khalil Raies <khalil.raies@check24.de>
2025-04-16 14:10:05 +0200

    Course 2 - Chap 3: Caching a twig variable

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -7,6 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class QuestionController extends AbstractController
 {
@@ -20,14 +22,17 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownParserInterface $markdownParser){
+    public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache){
         $answers = [
             'Make sure your cat is cutting `purrfectlyyyy` still',
             'Honestly, I like furry shoes better than MY Cat',
             'Maybe... try saying the spell backwards?',
         ];
         $question_text = 'I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
-        $parsedQuestionText = $markdownParser->transformMarkdown($question_text);
+
+        $parsedQuestionText = $cache->get('markdown_'.md5($question_text), function () use($question_text, $markdownParser) {
+            return $markdownParser->transformMarkdown($question_text);
+        });
         //dd($slug, $this);
         dump($slug, $this);
 
