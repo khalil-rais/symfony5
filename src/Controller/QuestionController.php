faa00150b905c6e71e43d06b773abb4c9154ca44
Khalil Raies <khalil.raies@check24.de>
2025-04-16 13:42:41 +0200

    Course 2 - Chap 1: Markdown Dependency Injection

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,17 +20,20 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug){
+    public function show($slug, MarkdownParserInterface $markdownParser){
         $answers = [
-            'Make sure your cat is cutting purrfectly still',
+            'Make sure your cat is cutting `purrfectlyyyy` still',
             'Honestly, I like furry shoes better than MY Cat',
             'Maybe... try saying the spell backwards?',
         ];
+        $question_text = 'I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
+        $parsedQuestionText = $markdownParser->transformMarkdown($question_text);
         //dd($slug, $this);
         dump($slug, $this);
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-',' ',$slug)),
+            'questionText' => $parsedQuestionText,
             'answers' => $answers,
         ]);
     }
