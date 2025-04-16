c13bb5208f9c9b814ef08bfc1a32a05fd2fbbfe1
Khalil Raies <khalil.raies@check24.de>
2025-04-16 15:49:56 +0200

    Course 2 - Chap 6: After Config Changes, we replaced file caching by apcu caching

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -34,7 +34,8 @@ class QuestionController extends AbstractController
             return $markdownParser->transformMarkdown($question_text);
         });
         //dd($slug, $this);
-        dump($slug, $this);
+        //dump($slug, $this);
+        //dump($cache);
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-',' ',$slug)),
