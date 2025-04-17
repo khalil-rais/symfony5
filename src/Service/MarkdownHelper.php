be1c94e41fb1233cd97fe010e1d02fe1dc6a04aa
Khalil Raies <khalil.raies@check24.de>
2025-04-17 09:33:33 +0200

    Course 2 - Chap 11: defining a non autowired service argument

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -9,11 +9,17 @@ class MarkdownHelper
 {
     private $markdownParser;
     private $cache;
-    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache){
+    private $isDebug;
+    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache, bool $isDebug){
         $this->markdownParser = $markdownParser;
         $this->cache = $cache;
+        $this->isDebug = $isDebug;
+        dump($isDebug);
     }
     public function parse(string $question_text): string{
+        if($this->isDebug){
+            return $this->markdownParser->transformMarkdown($question_text);
+        }
         return $this->cache->get('markdown_'.md5($question_text), function () use($question_text) {
             return $this->markdownParser->transformMarkdown($question_text);
         });
