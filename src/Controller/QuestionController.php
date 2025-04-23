0794c10609ea8c155fb0a526f439f5f0ff392fee
Khalil Raies <khalil.raies@check24.de>
2025-04-23 17:06:12 +0200

    Course 3 - Chap 8: Save data into the DB

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Sentry\State\HubInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Console\Question\Question;
+use App\Entity\Question;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Psr\Log\LoggerInterface;
@@ -19,6 +21,39 @@ class QuestionController extends AbstractController
         $this->logger = $logger;
         $this->isDebug = $isDebug;
     }
+
+    /**
+     * @Route("/questions/new")
+     */
+    public function new(EntityManagerInterface $entityManager)
+    {
+        $question = new Question();
+        $question->setName('Missing dress')
+        ->setSlug("missing-dress-" . rand(0, 1000))
+        ->setQuestion(<<<EOF
+Hi! So... I'm having a *weird* day. Yesterday, I cast a spell 
+to make my dishes wash themselves. But while I was casting it, 
+I slipped a little and I think 'I also hit my dress with the spell'.
+
+When I woke up this morning, I caught a quick glimpse of my dresses 
+opening the front door and walking out! I've been out all afternoon 
+(with no dresses mind you) searching for them.
+
+Does anyone have a spell to call your dresses back?
+EOF
+);
+        if(rand(1,10) > 2) {
+            $question->setAskedAt(new \DateTimeImmutable(sprintf('-%d days', rand(1, 100))));
+        }
+        $entityManager->persist($question);
+        $entityManager->flush();
+
+        return new Response(
+            sprintf("Well hallo! The shiny new question is id #%d, slug %s",
+                $question->getId(),
+                $question->getSlug()));
+    }
+
     /**
      * @Route("/", name="app_homepage")
      */
