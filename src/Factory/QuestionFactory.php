8cfb54601c63f5c7eceb2206832c4b828f67b22c
Khalil Raies <khalil.raies@check24.de>
2025-04-24 12:32:55 +0200

    Course 3 - Chap 20: Installing Foudry and Generating Question Factory

---
+++ src/Factory/QuestionFactory.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Question;
+use App\Repository\QuestionRepository;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+use Zenstruck\Foundry\Persistence\Proxy;
+use Zenstruck\Foundry\Persistence\ProxyRepositoryDecorator;
+
+/**
+ * @extends PersistentProxyObjectFactory<Question>
+ *
+ * @method        Question|Proxy                              create(array|callable $attributes = [])
+ * @method static Question|Proxy                              createOne(array $attributes = [])
+ * @method static Question|Proxy                              find(object|array|mixed $criteria)
+ * @method static Question|Proxy                              findOrCreate(array $attributes)
+ * @method static Question|Proxy                              first(string $sortedField = 'id')
+ * @method static Question|Proxy                              last(string $sortedField = 'id')
+ * @method static Question|Proxy                              random(array $attributes = [])
+ * @method static Question|Proxy                              randomOrCreate(array $attributes = [])
+ * @method static QuestionRepository|ProxyRepositoryDecorator repository()
+ * @method static Question[]|Proxy[]                          all()
+ * @method static Question[]|Proxy[]                          createMany(int $number, array|callable $attributes = [])
+ * @method static Question[]|Proxy[]                          createSequence(iterable|callable $sequence)
+ * @method static Question[]|Proxy[]                          findBy(array $attributes)
+ * @method static Question[]|Proxy[]                          randomRange(int $min, int $max, array $attributes = [])
+ * @method static Question[]|Proxy[]                          randomSet(int $number, array $attributes = [])
+ */
+final class QuestionFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Question::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'name' => self::faker()->text(255),
+            'question' => self::faker()->text(),
+            'slug' => self::faker()->text(100),
+            'votes' => self::faker()->randomNumber(),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Question $question): void {})
+        ;
+    }
+}
