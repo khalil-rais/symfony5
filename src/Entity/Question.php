5d290627b5b6fdb4f89e2b2e4dbde19240217e10
Khalil Raies <khalil.raies@check24.de>
2025-04-24 10:36:41 +0200

    Course 3 - Chap 16: Add a new Vote Parameter

--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -26,6 +26,9 @@ class Question
     #[ORM\Column(nullable: true)]
     private ?\DateTimeImmutable $askedAt = null;
 
+    #[ORM\Column]
+    private ?int $votes = 0;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -75,6 +78,18 @@ class Question
     public function setAskedAt(?\DateTimeImmutable $askedAt): static
     {
         $this->askedAt = $askedAt;
+
+        return $this;
+    }
+
+    public function getVotes(): int
+    {
+        return $this->votes;
+    }
+
+    public function setVotes(int $votes): static
+    {
+        $this->votes = $votes;
 
         return $this;
     }
