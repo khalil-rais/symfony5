7c17ce64c453716acbb46624317a762499fec602
Khalil Raies <khalil.raies@check24.de>
2025-04-29 14:30:03 +0200

    4-Doctrine Relations. 4.2-The ManyToOne Relation.  1-Mapping the Other Side of the Relation

---
+++ migrations/Version20250429122611.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250429122611 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE answer ADD question_id INT NOT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE answer ADD CONSTRAINT FK_DADD4A251E27F6BF FOREIGN KEY (question_id) REFERENCES question (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_DADD4A251E27F6BF ON answer (question_id)
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE answer DROP FOREIGN KEY FK_DADD4A251E27F6BF
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP INDEX IDX_DADD4A251E27F6BF ON answer
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE answer DROP question_id
+        SQL);
+    }
+}
