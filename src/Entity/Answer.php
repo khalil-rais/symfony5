b5e5b75cad11b48a7a0a1778a32385b62481d287
Khalil Raies <khalil.raies@check24.de>
2025-04-29 14:09:26 +0200

    4-Doctrine Relations. 4.1-Answer Entity. 1-Making the Answer Entity. Minor Adjustments

--- src/Entity/Answer.php
+++ src/Entity/Answer.php
@@ -5,10 +5,12 @@ namespace App\Entity;
 use App\Repository\AnswerRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 #[ORM\Entity(repositoryClass: AnswerRepository::class)]
 class Answer
 {
+    use TimestampableEntity;
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -21,7 +23,7 @@ class Answer
     private ?string $username = null;
 
     #[ORM\Column]
-    private ?int $votes = null;
+    private ?int $votes = 0;
 
     public function getId(): ?int
     {
