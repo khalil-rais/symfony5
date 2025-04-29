b3db86455b253cd3a7d6cff3613103d9dea8a0c2
Khalil Raies <khalil.raies@check24.de>
2025-04-29 16:26:49 +0200

    4-Doctrine Relations. 4.4-Relations in Foundry.  1-make:factory Answer

---
+++ src/Factory/AnswerFactory.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Answer;
+use App\Repository\AnswerRepository;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+use Zenstruck\Foundry\Persistence\Proxy;
+use Zenstruck\Foundry\Persistence\ProxyRepositoryDecorator;
+
+/**
+ * @extends PersistentProxyObjectFactory<Answer>
+ *
+ * @method        Answer|Proxy                              create(array|callable $attributes = [])
+ * @method static Answer|Proxy                              createOne(array $attributes = [])
+ * @method static Answer|Proxy                              find(object|array|mixed $criteria)
+ * @method static Answer|Proxy                              findOrCreate(array $attributes)
+ * @method static Answer|Proxy                              first(string $sortedField = 'id')
+ * @method static Answer|Proxy                              last(string $sortedField = 'id')
+ * @method static Answer|Proxy                              random(array $attributes = [])
+ * @method static Answer|Proxy                              randomOrCreate(array $attributes = [])
+ * @method static AnswerRepository|ProxyRepositoryDecorator repository()
+ * @method static Answer[]|Proxy[]                          all()
+ * @method static Answer[]|Proxy[]                          createMany(int $number, array|callable $attributes = [])
+ * @method static Answer[]|Proxy[]                          createSequence(iterable|callable $sequence)
+ * @method static Answer[]|Proxy[]                          findBy(array $attributes)
+ * @method static Answer[]|Proxy[]                          randomRange(int $min, int $max, array $attributes = [])
+ * @method static Answer[]|Proxy[]                          randomSet(int $number, array $attributes = [])
+ */
+final class AnswerFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Answer::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'content' => self::faker()->text(),
+            'createdAt' => self::faker()->dateTime(),
+            'question' => QuestionFactory::new(),
+            'updatedAt' => self::faker()->dateTime(),
+            'username' => self::faker()->text(255),
+            'votes' => self::faker()->randomNumber(),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Answer $answer): void {})
+        ;
+    }
+}
