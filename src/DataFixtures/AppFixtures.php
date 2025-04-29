fdc8785cb3164a423a874edc683240c6ffb3ce5e
Khalil Raies <khalil.raies@check24.de>
2025-04-29 16:24:10 +0200

    4-Doctrine Relations. 4.3-Saving Relations.  1-Creating some Dummy Question and Answer Objects

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Answer;
 use App\Entity\Question;
 use App\Factory\QuestionFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -18,5 +19,21 @@ class AppFixtures extends Fixture
         QuestionFactory::new()
             ->unpublished()
             ->createMany(5);
+
+        $answer = new Answer();
+        $answer->setContent('This question is the best! I whsih I knew the answer');
+        $answer->setUsername ('weaverryan') ;
+
+        $question = new Question();
+        $question->setName('How to up-disappear you wallet.');
+        $question->setQuestion('... I should not have done this...');
+
+        $answer->setQuestion($question);
+
+        $manager->persist($answer);
+        $manager->persist($question);
+
+        $manager->flush();
+
     }
 }
