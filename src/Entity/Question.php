02430613f2baee066f122aa8b88a2e270e2125ae
Khalil Raies <khalil.raies@check24.de>
2025-05-02 15:06:40 +0200

    4-Doctrine Relations. 4.9-Relation OrderBy & fetch=EXTRA_LAZY. 1-Ordering question->getAnswers() with ORM\OrderBy

--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -38,6 +38,7 @@ class Question
     private ?int $votes = 0;
 
     #[ORM\OneToMany(mappedBy: 'question', targetEntity: Answer::class)]
+    #[ORM\OrderBy(['createdAt' => 'DESC'])]
     private Collection $answers;
 
     public function __construct()
