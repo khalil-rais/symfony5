5b263a8697b6e19ebdf3b8c9bedfe8e1427aa0f1
Khalil Raies <khalil.raies@check24.de>
2025-05-05 16:27:28 +0200

    4-Doctrine Relations. 4.17-ManyToMany Relation

--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -43,9 +43,13 @@ class Question
     #[ORM\OrderBy(['createdAt' => 'DESC'])]
     private Collection $answers;
 
+    #[ORM\ManyToMany(targetEntity: Tag::class, inversedBy: 'questions')]
+    private Collection $tags;
+
     public function __construct()
     {
         $this->answers = new ArrayCollection();
+        $this->tags = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -164,6 +168,30 @@ class Question
                 $answer->setQuestion(null);
             }
         }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Tag>
+     */
+    public function getTags(): Collection
+    {
+        return $this->tags;
+    }
+
+    public function addTag(Tag $tag): static
+    {
+        if (!$this->tags->contains($tag)) {
+            $this->tags->add($tag);
+        }
+
+        return $this;
+    }
+
+    public function removeTag(Tag $tag): static
+    {
+        $this->tags->removeElement($tag);
 
         return $this;
     }
