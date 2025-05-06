2517a1c2cb986af098d44faf2b58b2630a332d5b
Khalil Raies <khalil.raies@check24.de>
2025-05-06 13:39:03 +0200

    4-Doctrine Relations. 4.21-ManyToMany... with Extra Fields on the Join Table? 2-Generating the Join Entity

---
+++ migrations/Version20250506113517.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250506113517 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE question_tag DROP FOREIGN KEY FK_339D56FB1E27F6BF
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE question_tag DROP FOREIGN KEY FK_339D56FBBAD26311
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE question_tag ADD id INT AUTO_INCREMENT NOT NULL, ADD tagged_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', DROP PRIMARY KEY, ADD PRIMARY KEY (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE question_tag ADD CONSTRAINT FK_339D56FB1E27F6BF FOREIGN KEY (question_id) REFERENCES question (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE question_tag ADD CONSTRAINT FK_339D56FBBAD26311 FOREIGN KEY (tag_id) REFERENCES tag (id)
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE question_tag MODIFY id INT NOT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE question_tag DROP FOREIGN KEY FK_339D56FB1E27F6BF
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE question_tag DROP FOREIGN KEY FK_339D56FBBAD26311
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP INDEX `PRIMARY` ON question_tag
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE question_tag DROP id, DROP tagged_at
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE question_tag ADD CONSTRAINT FK_339D56FB1E27F6BF FOREIGN KEY (question_id) REFERENCES question (id) ON UPDATE NO ACTION ON DELETE CASCADE
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE question_tag ADD CONSTRAINT FK_339D56FBBAD26311 FOREIGN KEY (tag_id) REFERENCES tag (id) ON UPDATE NO ACTION ON DELETE CASCADE
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE question_tag ADD PRIMARY KEY (question_id, tag_id)
+        SQL);
+    }
+}
--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -43,13 +43,13 @@ class Question
     #[ORM\OrderBy(['createdAt' => 'DESC'])]
     private Collection $answers;
 
-    #[ORM\ManyToMany(targetEntity: Tag::class, inversedBy: 'questions')]
-    private Collection $tags;
+    #[ORM\OneToMany(mappedBy: 'question', targetEntity: QuestionTag::class)]
+    private Collection $questionTags;
 
     public function __construct()
     {
         $this->answers = new ArrayCollection();
-        $this->tags = new ArrayCollection();
+        $this->questionTags = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -173,25 +173,31 @@ class Question
     }
 
     /**
-     * @return Collection<int, Tag>
+     * @return Collection<int, QuestionTag>
      */
-    public function getTags(): Collection
+    public function getQuestionTags(): Collection
     {
-        return $this->tags;
+        return $this->questionTags;
     }
 
-    public function addTag(Tag $tag): static
+    public function addQuestionTag(QuestionTag $questionTag): static
     {
-        if (!$this->tags->contains($tag)) {
-            $this->tags->add($tag);
+        if (!$this->questionTags->contains($questionTag)) {
+            $this->questionTags->add($questionTag);
+            $questionTag->setQuestion($this);
         }
 
         return $this;
     }
 
-    public function removeTag(Tag $tag): static
+    public function removeQuestionTag(QuestionTag $questionTag): static
     {
-        $this->tags->removeElement($tag);
+        if ($this->questionTags->removeElement($questionTag)) {
+            // set the owning side to null (unless already changed)
+            if ($questionTag->getQuestion() === $this) {
+                $questionTag->setQuestion(null);
+            }
+        }
 
         return $this;
     }
