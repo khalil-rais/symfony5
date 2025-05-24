78e827d738e17fb751d039e1781b790dfee99b67
Khalil Raies <khalil.raies@check24.de>
2025-05-24 21:32:25 +0200

    5-Symfony 5 Security: Authenticators. 5.6-Authenticator & The Passport 0-Reading the authentication inputs

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -2,11 +2,14 @@
 
 namespace App\Security;
 
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 
 class LoginFormAuthenticator extends AbstractAuthenticator
@@ -18,7 +21,15 @@ class LoginFormAuthenticator extends AbstractAuthenticator
 
     public function authenticate(Request $request): Passport
     {
-        dd('Authenticate');
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+
+        return new Passport (
+            new UserBadge ($email),
+            new CustomCredentials(function($credentials, User $user) {
+                dd ($credentials, $user);
+            }, $password)
+        ) ;
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
