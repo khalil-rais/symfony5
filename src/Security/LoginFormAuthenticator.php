1e33ebdc3bcf5dc776ae426b96effeff1e0577e3
Khalil Raies <khalil.raies@check24.de>
2025-05-29 22:30:20 +0200

    5-Symfony 5 Security: Authenticators. 5.18-AbstractLoginFormAuthenticator & Redirecting to Previous URL  0-Redirect the the target after login

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -21,9 +21,12 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordC
 use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 {
+    use TargetPathTrait;
+
     private UserRepository $userRepository;
     private RouterInterface $router;
 
@@ -62,6 +65,10 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
+        if($target = $this->getTargetPath($request->getSession(), $firewallName)){
+            return new RedirectResponse($target);
+        }
+        
         return new RedirectResponse(
             $this->router->generate('app_homepage')
         );
