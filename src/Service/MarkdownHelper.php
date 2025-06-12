7f02ca838c5bc89e8578cfe6377534109d4e8db8
Khalil Raies <khalil.raies@check24.de>
2025-06-12 14:17:48 +0200

    5-Symfony 5 Security: Authenticators. 5.25-Custom User Methods & the User in a Service 0-Accessing the User Attributes from the Service through the Security Component

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -4,6 +4,7 @@ namespace App\Service;
 
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Contracts\Cache\CacheInterface;
 
 class MarkdownHelper
@@ -12,16 +13,24 @@ class MarkdownHelper
     private $cache;
     private $isDebug;
     private $logger;
+    private Security $security;
 
-    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache, bool $isDebug, LoggerInterface $markdownLogger){
+    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache, bool $isDebug, LoggerInterface $markdownLogger, Security $security){
         $this->markdownParser = $markdownParser;
         $this->cache = $cache;
         $this->isDebug = $isDebug;
         $this->logger = $markdownLogger;
+        $this->security = $security;
     }
     public function parse(string $question_text): string{
         if(stripos($question_text, 'cat') !== false){
             $this->logger->info('Meow!');
+        }
+
+        if($this->security->getUser()){
+            $this->logger->info('Rendering markdown for {user}', [
+                'user' => $this->security->getUser()->getUserIdentifier(),
+            ]);
         }
 
         if($this->isDebug){
