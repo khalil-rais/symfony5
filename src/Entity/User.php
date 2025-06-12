93f99b9e2c28b49194611152675547db251df5f8
Khalil Raies <khalil.raies@check24.de>
2025-06-12 14:05:28 +0200

    5-Symfony 5 Security: Authenticators. 5.25-Custom User Methods & the User in a Service 0-Generating in twig the avatar uri from the user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -140,5 +140,16 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $this->plainPassword = $plainPassword;
     }
 
+    public function getAvatarUri(int $size = 32): string
+    {
+        // https://ui-avatars.com/api/?name={{ app.user.firstName | url_encode }}&size=32&background=random
+        return 'https://ui-avatars.com/api/?' . http_build_query([
+            'name' => $this->getFirstname() ?: $this->getEmail(),
+            'size' => $size,
+            'background' => 'random',
+        ]);
+
+    }
+
 
 }
