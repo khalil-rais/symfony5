124afa3918bb9d33aebcbb847cfeb4615fb823a9
Khalil Raies <khalil.raies@check24.de>
2025-06-13 16:09:38 +0200

    5-Symfony 5 Security: Authenticators. 5.35-Custom Voter 0-Setting the Voter to check the user against the question author

--- src/Security/Voter/QuestionVoter.php
+++ src/Security/Voter/QuestionVoter.php
@@ -2,6 +2,8 @@
 
 namespace App\Security\Voter;
 
+use App\Entity\Question;
+use App\Entity\User;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -15,29 +17,27 @@ class QuestionVoter extends Voter
     {
         // replace with your own logic
         // https://symfony.com/doc/current/security/voters.html
-        return in_array($attribute, [self::EDIT, self::VIEW])
+        return in_array($attribute, ['EDIT'])
             && $subject instanceof \App\Entity\Question;
     }
 
     protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
     {
+        /** @var User $user */
         $user = $token->getUser();
         // if the user is anonymous, do not grant access
         if (!$user instanceof UserInterface) {
             return false;
         }
 
+        if (!$subject instanceof Question) {
+            throw new \Exception ( 'Wrong type somehow passed');
+        }
+
         // ... (check conditions and return true to grant permission) ...
         switch ($attribute) {
-            case self::EDIT:
-                // logic to determine if the user can EDIT
-                // return true or false
-                break;
-
-            case self::VIEW:
-                // logic to determine if the user can VIEW
-                // return true or false
-                break;
+            case 'EDIT':
+                return $user === $subject->getOwner();
         }
 
         return false;
