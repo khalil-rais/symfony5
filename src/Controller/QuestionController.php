a42a31e2a34c6a0a3f7423b7d7b9186a2aaa8081
Khalil Raies <khalil.raies@check24.de>
2025-06-13 14:50:05 +0200

    5-Symfony 5 Security: Authenticators. 5.33-Leveraging the Question Owner 0-A first approach to set access rights for edit question

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -76,6 +76,12 @@ class QuestionController extends AbstractController
      */
     public function edit(Question $question){
 
+        $this->denyAccessUnlessGranted ('IS_AUTHENTICATED_REMEMBERED');
+
+        if ($question->getOwner () !== $this->getUser()) {
+            throw $this->createAccessDeniedException( 'You are not the owner!');
+        }
+
         return $this->render('question/edit.html.twig', [
             'question' => $question
         ]);
