3c99cd8b236e51f33c9ea9695223e24f4f7eaddd
Khalil Raies <khalil.raies@check24.de>
2025-06-16 11:14:26 +0200

    5-Symfony 5 Security: Authenticators. 5.42-2 Factor Authentication & Authentication Tokens 0-Installing the 2fa library

--- config/bundles.php
+++ config/bundles.php
@@ -23,4 +23,5 @@ return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
 ];
