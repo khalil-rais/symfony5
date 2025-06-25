6482cb75df91766277a1726cab2c826b77ac63b8
Khalil Raies <khalil.raies@check24.de>
2025-06-25 17:04:21 +0200

    6.2-Hooking into Symfony with an Event Subscriber 4-Getting information from the event argument

--- src/EventListener/UserEventSubscriber.php
+++ src/EventListener/UserEventSubscriber.php
@@ -16,8 +16,9 @@ class UserEventSubscriber implements EventSubscriberInterface
     }
     public function onKernelRequest(RequestEvent $event)
     {
-        dd($event);
-        $this->logger->info('I\'m logging SUPER early on the request');
+        $request = $event->getRequest ();
+        $userAgent = $request->headers->get('User-Agent');
+        $this->logger->info(sprintf('The User-Agent is "%s"', $userAgent));
     }
 
     public static function getSubscribedEvents(){
