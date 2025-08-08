eb2d8fb9543208f0f16f7306aba0d3eef34250f7
Khalil Raies <khalil.raies@check24.de>
2025-08-08 19:31:30 +0200

    6.26-Sub Request Attributes 1-Passing isMac through the Request Attributes

--- src/EventListener/UserEventSubscriber.php
+++ src/EventListener/UserEventSubscriber.php
@@ -4,6 +4,7 @@ namespace App\EventListener;
 
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 
@@ -24,11 +25,22 @@ class UserEventSubscriber implements EventSubscriberInterface
         $request = $event->getRequest ();
         $userAgent = $request->attributes->get('isMac');
         $this->logger->info(sprintf('The User-Agent is "%s"', $userAgent));
+        $request->attributes->set('_isMac', $this->isMac($request));
     }
 
     public static function getSubscribedEvents(){
         return [
             RequestEvent::class => 'onKernelRequest',
         ];
+    }
+
+    private function isMac(Request $request): bool
+    {
+        if ($request->query->has('mac')) {
+            return $request->query->getBoolean('mac');
+        }
+        $userAgent = $request->headers->get('User-Agent');
+
+        return stripos($userAgent, 'Mac') !== false;
     }
 }
