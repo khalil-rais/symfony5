722333010a9bda815f33d7aada2ffe8ac3c5c71f
Khalil Raies <khalil.raies@check24.de>
2025-10-07 17:19:32 +0200

    Chapter 8.17: Tracking Messages with Middleware & a Stamp

---
+++ src/Messenger/UniqueIdStamp.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Messenger;
+
+use Symfony\Component\Messenger\Stamp\StampInterface;
+
+class UniqueIdStamp implements StampInterface
+{
+    private $uniqueId;
+    public function __construct()
+    {
+        $this->uniqueId = uniqid();
+    }
+    public function getUniqueId(): string
+    {
+        return $this->uniqueId;
+    }
+}
