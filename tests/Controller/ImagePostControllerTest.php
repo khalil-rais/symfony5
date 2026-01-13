b55b3c75526795527ac18fcb2c77796bba8977c8
Khalil Raies <khalil.raies@check24.de>
2026-01-13 13:00:31 +0100

    Chapter 8.23: Testing with the in-memory Transport: Fetching the Transport Service

--- tests/Controller/ImagePostControllerTest.php
+++ tests/Controller/ImagePostControllerTest.php
@@ -4,6 +4,7 @@ namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Messenger\Transport\InMemoryTransport;
 
 class ImagePostControllerTest extends WebTestCase
 {
@@ -18,5 +19,9 @@ class ImagePostControllerTest extends WebTestCase
             'file' => $uploadedFile
         ]);
         $this->assertResponseIsSuccessful();
+
+        /** @var InMemoryTransport $transport */
+        $transport = self::$container->get('messenger.transport.async_priority_high');
+        $this->assertCount(1, $transport->get());
     }
 }
