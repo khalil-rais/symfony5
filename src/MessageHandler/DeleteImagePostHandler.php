b22a33c6073a707e1577885096cc0bd773f004f7
Khalil Raies <khalil.raies@check24.de>
2026-01-14 17:29:59 +0100

    Chapter 8.28: Dispatching the Event & No Handlers

--- src/MessageHandler/DeleteImagePostHandler.php
+++ src/MessageHandler/DeleteImagePostHandler.php
@@ -6,14 +6,19 @@ use App\Message\DeleteImagePost;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
+use App\Message\Event\ImagePostDeletedEvent;
 
 class DeleteImagePostHandler implements MessageHandlerInterface
 {
-    private $messageBus;
-    private $entityManager;
 
-    public function __construct(MessageBusInterface $messageBus, EntityManagerInterface $entityManager){
-        $this->messageBus = $messageBus;
+    /*
+     Inside DeleteImagePostHandler, change the argument to $eventBus.
+     I don't have to, but I'm also going to rename the property to $eventBus for clarity.
+     */
+    private $eventBus;
+    private $entityManager;
+    public function __construct(MessageBusInterface $eventBus, EntityManagerInterface $entityManager){
+        $this->eventBus = $eventBus;
         $this->entityManager = $entityManager;
     }
 
@@ -42,6 +47,20 @@ class DeleteImagePostHandler implements MessageHandlerInterface
     and then remove the DeletePhotoFile use statement on top.
  */
 //        $this->messageBus->dispatch(new DeletePhotoFile($filename));
+        /*
+         Inside __invoke(), it's really the same as before:
+        $this->eventBus->dispatch() with new ImagePostDeletedEvent() passing that $filename.
+         */
+        $this->eventBus->dispatch(new ImagePostDeletedEvent($filename));
+        /*
+            That's it! The end result of all of this work
+            was to do the same thing as before,
+            but with some renaming to match the "event bus" pattern.
+            The handler performs its primary task
+            - deleting the record from the database -
+            then dispatches an event that says:
+            “An image post was just deleted! If anyone cares... do something!”
+         */
 
     }
 }
