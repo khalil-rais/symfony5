81bba52a5be6ac53496e63be62bedd9397274947
Khalil Raies <khalil.raies@check24.de>
2026-01-14 12:47:35 +0100

    Chapter 8.27: Creating & Handling Events: The Secondary Task of DeleteImagePostHandler

--- src/MessageHandler/DeleteImagePostHandler.php
+++ src/MessageHandler/DeleteImagePostHandler.php
@@ -3,7 +3,6 @@
 namespace App\MessageHandler;
 
 use App\Message\DeleteImagePost;
-use App\Message\DeletePhotoFile;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
@@ -26,8 +25,23 @@ class DeleteImagePostHandler implements MessageHandlerInterface
 
         $this->entityManager->remove($imagePost);
         $this->entityManager->flush();
-
-        $this->messageBus->dispatch(new DeletePhotoFile($filename));
+/*
+    We already have a situation like this!
+    Look at DeleteImagePost and then DeleteImagePostHandler.
+    The "main" job for this handler is to remove this ImagePost from the database.
+    But it also has a second task: deleting the underlying file from the filesystem.
+    To do that, well, we're dispatching a second command - DeletePhotoFile -
+    and its handler deletes the file.
+    This is the event pattern!
+    Well, it's almost the event pattern.
+    The only difference is the naming: DeletePhotoFile sounds like a "command".
+    Instead of "commanding" the system to do something,
+    an event is more of an "announcement" that something did happen.
+    To fully understand this, let's back up and re-implement all of this fresh.
+    Comment out the $messageBus->dispatch() call
+    and then remove the DeletePhotoFile use statement on top.
+ */
+//        $this->messageBus->dispatch(new DeletePhotoFile($filename));
 
     }
 }
