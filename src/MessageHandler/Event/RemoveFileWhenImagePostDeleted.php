2ca1cbc33c800569d9cb63a480fa2e0411ca02c9
Khalil Raies <khalil.raies@check24.de>
2026-01-15 17:07:30 +0100

    Chapter 8.28: Dispatching the Event & No Handlers: Events can have No Handlers

--- src/MessageHandler/Event/RemoveFileWhenImagePostDeleted.php
+++ src/MessageHandler/Event/RemoveFileWhenImagePostDeleted.php
@@ -24,7 +24,26 @@ and then create our beloved public function __invoke()
 with the type-hint for the event class: ImagePostDeletedEvent $event.
  */
 use App\Photo\PhotoFileManager;
-class RemoveFileWhenImagePostDeleted implements MessageHandlerInterface
+/*
+    It is possible to dispatch an event with no handlers.
+    Open RemoveFileWhenImagePostDeleted
+    and take off the implements MessageHandleInterface part.
+ */
+/*
+    I'm doing this temporarily to see what happens if Symfony sees zero handlers for an event.
+    Restart the worker, it works then it logs:
+    CRITICAL error!
+    15:54:07 INFO      [messenger] Received message App\Message\Event\ImagePostDeletedEvent ["message" => App\Message\Event\ImagePostDeletedEvent^ { …},"class" => "App\Message\Event\ImagePostDeletedEvent"]
+    15:54:07 CRITICAL  [messenger] Error thrown while handling message App\Message\Event\ImagePostDeletedEvent. Removing from transport after 3 retries. Error: "No handler for message "App\Message\Event\ImagePostDeletedEvent"." ["message" => App\Message\Event\ImagePostDeletedEvent^ { …},"class" => "App\Message\Event\ImagePostDeletedEvent","retryCount" => 3,"error" => "No handler for message "App\Message\Event\ImagePostDeletedEvent".","exception" => Symfony\Component\Messenger\Exception\NoHandlerForMessageException^ { …}]
+
+    By default, Messenger requires each message to have at least one handler.
+    That's to help us avoid silly mistakes.
+    But for an event bus we do want to allow zero handlers.
+    Again this is more of a philosophical problem than a real one:
+    it's unlikely you'll decide to dispatch events that have no handlers.
+    But, let's see how to fix it!
+ */
+class RemoveFileWhenImagePostDeleted
 {
     private $photoFileManager;
     public function __construct(PhotoFileManager $photoFileManager){
