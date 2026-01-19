e1a4e586548796d469f48aa72910fd53deefb05d
Khalil Raies <khalil.raies@check24.de>
2026-01-19 17:57:13 +0100

    Chapter 8.32: Query Bus: Dispatching the Message

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,14 +4,22 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/")
      */
-    public function homepage()
+    public function homepage(MessageBusInterface $queryBus)
     {
+        /*
+            We can get the main command.bus
+            by using the MessageBusInterface type-hint with any argument name.
+            To get the query bus, we need to use that type-hint and name the argument: $queryBus.
+            Do that: MessageBusInterface $queryBus.
+            Inside the function, say $envelope = $queryBus->dispatch(new GetTotalImageCount()).
+         */
         return $this->render('main/homepage.html.twig');
     }
 }
