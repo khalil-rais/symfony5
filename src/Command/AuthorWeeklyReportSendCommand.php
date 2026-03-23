67d40b4c256551fd6cad4b4f8890e65845417e9f
Khalil Raies <khalil.raies@check24.de>
2026-03-23 17:29:00 +0100

    Chapter 9.12: Let's Make a Console Command: Autowiring Services into the Command

--- src/Command/AuthorWeeklyReportSendCommand.php
+++ src/Command/AuthorWeeklyReportSendCommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use App\Repository\UserRepository;
 
 /*
 Let's bootstrap the command the lazy way.
@@ -32,24 +33,69 @@ and I'll change the description:
 )]
 class AuthorWeeklyReportSendCommand extends Command
 {
+    private $userRepository;
+    /*
+        Back in the command, let's autowire the repository by adding a constructor.
+        This is one of the rare cases
+        where we have a parent class
+        and the parent class has a constructor.
+        I'll go to the Code -> Generate menu - or Command + N on a Mac - and select "Override methods"
+        to override the constructor.
+        Notice that this added a $name argument -
+        that's an argument in the parent constructor -
+        and it called the parent constructor.
+        That's important: the parent class needs to set some stuff up.
+        But, we don't need to pass the command name:
+        Symfony already gets that from a static property on our class.
+        Instead, make the first argument: UserRepository $userRepository.
+        Hit Alt + Enter and select "Initialize fields"
+        to create that property and set it. Perfect.
+    */
+    public function __construct(UserRepository $userRepository)
+    {
+        parent::__construct(null);
+
+        $this->userRepository = $userRepository;
+    }
+
     protected function configure(): void
     {
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        /*
+            Next, in execute(), clear everything out except for the $io variable,
+            which is a nice little object that helps us print things and interact with the user in a pretty way.
+            Start with $authors = $this->userRepository->findAllSubscribedToNewsletter().
+            Well, this really returns all users not just authors -
+            but we'll filter them out in a minute.
+            To be extra fancy, let's add a progress bar!
+            Start one with $io->progressStart().
+            Then, foreach over $authors as $author,
+            and advance the progress inside.
+            Oh, and of course, for progressStart(),
+            I need to tell it how many data points we're going to advance.
+            Use count($authors).
+            Leave the inside of the foreach empty for now, and after, say $io->progressFinish().
+            Finally, for a big happy message, add $io->success()
+            “Weekly reports were sent to authors!”
+            Brilliant! We're not doing anything yet
+            but let's try it!
+            Copy the command name, find your terminal, and do it!
+            php bin/console app:author-weekly-report:send
+            Super fast!
+         */
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
-
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+        $authors = $this->userRepository
+            ->findAllSubscribedToNewsletter();
+        $io->progressStart(count($authors));
+        foreach ($authors as $author) {
+            $io->progressAdvance();
         }
+        $io->progressFinish();
 
-        if ($input->getOption('option1')) {
-            // ...
-        }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $io->success('Weekly reports were sent to authors!');
 
         return Command::SUCCESS;
     }
