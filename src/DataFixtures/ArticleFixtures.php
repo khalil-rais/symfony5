4da03232a269bbdc9d23e4559ad611b233b4382d
Khalil Raies <khalil.raies@check24.de>
2026-05-21 18:06:08 +0200

    Chapter 10.13: File Uploads and Data Fixtures: Cleanup into a Private Method

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -71,65 +71,11 @@ EOF
                 $article->setPublishedAt($this->faker->dateTimeBetween('-100 days', '-1 days'));
             }
             /*
-                Here's the idea: we'll use the UploaderHelper down here,
-                point it at one of these 3 files,
-                and have it, sort of, "fake" upload it.
-                Start with $randomImage =, copy the faker code, and paste.
-                This is now one of the three random image filenames.
+                Back up top, delete all this stuff and say $imageFilename = $this->fakeUploadImage().
              */
-            $randomImage = $this->faker->randomElement(self::$articleImages);
+            $imageFilename = $this->fakeUploadImage();
             /*
-                Woh! The file src/DataFixtures/images/asteroid.jpeg does not exist? Hmm.
-                Check this out: it did upload two files before going all "explody" on us.
-                Oh, but those original files are missing! Of course!
-                We're using $file->move().
-                So it is working, but instead of copying the files, it's moving them,
-                and the originals are disappearing.
-                Let's get those files back. Run:
-
-                git status
-
-                And undelete them with:
-                git checkout src/DataFixtures/images
-                2 Pfade vom Index aktualisiert
-
-                Much better. Let's clean out the uploads directory again.
-
-                We do want to use $file->move() because we do want to move the uploaded file in normal circumstances.
-                So, to get around this, in the fixtures,
-                let's copy the original file to a temporary spot.
-                Start with $fs = new Filesystem() -
-                that's a handy object for doing filesystem operations.
-             */
-            $fs = new Filesystem();
-            /*
-                Next, $targetPath = sys_get_temp_dir().'/'.$randomImage.
-                And then use $fs->copy().
-                We want to copy the original file path into $targetPath
-             */
-            $targetPath = sys_get_temp_dir().'/'.$randomImage;
-            $fs->copy(__DIR__.'/images/'.$randomImage, $targetPath, true);
-            /*
-                That's ok! It just means we need to dig deeper!
-                Go back into UploaderHelper.
-                Hold Command or Ctrl and click to open the UploadedFile class.
-                This lives in the Symfony\HttpFoundation\File namespace
-                and extends a class called File that lives in the same directory.
-                The File class is awesome: it simply represents any file on your filesystem,
-                regardless of whether it's an uploaded file or just a normal file.
-                And, if you look closely, the vast majority of the methods we've been using come from this class - not from UploadedFile.
-                And we can create a File object outside of an upload context.
-                So back in ArticleFixtures, instead of creating a new UploadedFile(),
-                say new File() - the one from HttpFoundation.
-                Pass this the path to the random image: __DIR__.'/images/' and then $randomImage,
-                which will be one of these image filenames.
-             */
-            /*
-                Inside File, pass the temporary path.
-             */
-            $imageFilename = $this->uploaderHelper->uploadArticleImage(new File($targetPath));
-            /*
-                Ok, let's try it again!
+                Let's run those fixtures one more time!
                 php bin/console doctrine:fixtures:load
                  Careful, database "main" will be purged. Do you want to continue? (yes/no) [no]:
                  > yes
@@ -139,34 +85,15 @@ EOF
                    > loading App\DataFixtures\UserFixture
                    > loading App\DataFixtures\ArticleFixtures
                    > loading App\DataFixtures\CommentFixture
-
-                No error, our original files still exist
-                and we have a directory full of, fake uploaded files.
-                Now try the homepage. Beautiful.
-                What I really love about this is that we're not doing anything fancy or tricky in our fixtures:
-                we're literally using our upload system.
-             */
-            /*
-                Now, take $imageFilename - that'll be whatever the final filename is on the system after moving it,
-                and set that onto the entity.
-             */
-            /*
-                Next, in UploaderHelper, what I'd like to do is call uploadArticleImage()
-                and basically say:
-                “Hey! Pretend like asteroid.jpeg is a file that was just uploaded.
-                And... ya know... do all your normal stuff and move it into the uploads/ directory.”
-                This is easier than you think:
-                in the fixtures class, set $imageFilename to $this->uploaderHelper->uploadArticleImage().
-                What I want to do is now say new UploadedFile()
-                and point it at one of the images.
-                The problem is that you can't really create a fake UploadedFile object.
-                Internally, it's bound to the PHP uploading process
-                - weird stuff will happen if you try to create one outside of that context.
+                When it finishes, we have some new files and the homepage is shiny!
+                That's a solid fixture system.
+                Next: we'll take our first step towards storing uploaded files in the cloud by integrating the
+                gorgeous Flysystem library.
              */
 
             $article->setAuthor($this->getRandomReference('main_users'))
                 ->setHeartCount($this->faker->numberBetween(5, 100))
-                ->setImageFilename($this->faker->randomElement(self::$articleImages))
+                ->setImageFilename($imageFilename)
             ;
 
             $tags = $this->getRandomReferences('main_tags', $this->faker->numberBetween(0, 5));
@@ -186,5 +113,25 @@ EOF
             TagFixture::class,
             UserFixture::class,
         ];
+    }
+
+    /*
+        Though, I don't love having all of this logic right in the middle of this already-long function:
+        it's not super obvious what it does.
+        Let's do some cleanup: copy all of this.
+        And at the bottom, create a new private function fakeUploadImage() that will return a string.
+     */
+    private function fakeUploadImage(): string
+    {
+        /*
+            Paste all that logic and return the $this->uploaderHelper line.
+            It selects a random image, uploads it and returns the path.
+         */
+        $randomImage = $this->faker->randomElement(self::$articleImages);
+        $fs = new Filesystem();
+        $targetPath = sys_get_temp_dir().'/'.$randomImage;
+        $fs->copy(__DIR__.'/images/'.$randomImage, $targetPath, true);
+        return $this->uploaderHelper
+            ->uploadArticleImage(new File($targetPath));
     }
 }
