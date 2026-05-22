4c24830d66920b99cddd237e8d20a22010b0025a
Khalil Raies <khalil.raies@check24.de>
2026-05-22 15:19:24 +0200

    Chapter 10.14: Flysystem: Filesystem Abstraction: Adapters and Filesystems

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
 ];
