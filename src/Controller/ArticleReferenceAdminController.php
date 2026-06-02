b0978be0eafba40f114c74c42f3956011c599205
Khalil Raies <khalil.raies@check24.de>
2026-06-02 16:35:49 +0200

    Chapter 10.20: Uploading References

---
+++ src/Controller/ArticleReferenceAdminController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+class ArticleReferenceAdminController extends BaseController
+{
+    /*
+        Unlike the main form on this page,
+        this form will submit to a different endpoint.
+        And instead of continuing to put more things into ArticleAdminController,
+        let's create a new controller for everything related to article references: ArticleReferenceAdminController.
+        Extend BaseController - that's just a small base controller we created in our Symfony series:
+        it extends the normal AbstractController. So nothing magic happening there.
+     */
+
+}
