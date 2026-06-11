5595d9f2fa088faa57f53a4260176bab398dca2e
Khalil Raies <khalil.raies@check24.de>
2026-06-11 13:48:35 +0200

    Chapter 10.26: API Endpoint & Errors with Dropzone: Displaying Errors Correctly: missing js code from source file

--- public/js/admin_article_form.js
+++ public/js/admin_article_form.js
@@ -1,3 +1,8 @@
+import $ from 'jquery';
+import Dropzone from 'dropzone';
+import 'dropzone/dist/dropzone.css'
+import Sortable from 'sortablejs';
+
 /*
     We could fix this in the controller
     but we can also configure Dropzone to use the reference key.
@@ -13,17 +18,18 @@
 Dropzone.autoDiscover = false;
 
 $(document).ready(function() {
-    /*
-        Try it out - close the extra tab and refresh.
-        Hmm... still there?
-        Maybe a force refresh?
-        Now it's gone.
-        The dropzone class still gives us some styling,
-        but it's not functional anymore.
-        To get it working again, inside the document.ready(),
-        call a new initializeDropzone() function.
-     */
     initializeDropzone();
+    const $autoComplete = $('.js-user-autocomplete');
+    if (!$autoComplete.is(':disabled')) {
+        import('./components/algolia-autocomplete').then((autocomplete) => {
+            autocomplete.default($autoComplete, 'users', 'email');
+        });
+    }
+    const $referenceList = $('.js-reference-list');
+    if ($referenceList[0]) {
+        var referenceList = new ReferenceList($('.js-reference-list'));
+        initializeDropzone(referenceList);
+    }
     var $locationSelect = $('.js-article-form-location');
     var $specificLocationTarget = $('.js-specific-location-target');
 
@@ -49,6 +55,98 @@ $(document).ready(function() {
         });
     });
 });
+
+// todo - use Webpack Encore so ES6 syntax is transpiled to ES5
+class ReferenceList
+{
+    constructor($element) {
+        var stuff = new WeakSet([]);
+
+        this.$element = $element;
+        this.sortable = Sortable.create(this.$element[0], {
+            handle: '.drag-handle',
+            animation: 150,
+            onEnd: () => {
+                $.ajax({
+                    url: this.$element.data('url')+'/reorder',
+                    method: 'POST',
+                    data: JSON.stringify(this.sortable.toArray())
+                });
+            }
+        });
+        this.references = [];
+        this.render();
+
+        this.$element.on('click', '.js-reference-delete', (event) => {
+            this.handleReferenceDelete(event);
+        });
+
+        this.$element.on('blur', '.js-edit-filename', (event) => {
+            this.handleReferenceEditFilename(event);
+        });
+
+        $.ajax({
+            url: this.$element.data('url')
+        }).then(data => {
+            this.references = data;
+            this.render();
+        })
+    }
+
+    addReference(reference) {
+        this.references.push(reference);
+        this.render();
+    }
+
+    handleReferenceDelete(event) {
+        const $li = $(event.currentTarget).closest('.list-group-item');
+        const id = $li.data('id');
+        $li.addClass('disabled');
+
+        $.ajax({
+            url: '/admin/article/references/'+id,
+            method: 'DELETE'
+        }).then(() => {
+            this.references = this.references.filter(reference => {
+                return reference.id !== id;
+            });
+            this.render();
+        });
+    }
+
+    handleReferenceEditFilename(event) {
+        const $li = $(event.currentTarget).closest('.list-group-item');
+        const id = $li.data('id');
+        const reference = this.references.find(reference => {
+            return reference.id === id;
+        });
+        reference.originalFilename = $(event.currentTarget).val();
+
+        $.ajax({
+            url: '/admin/article/references/'+id,
+            method: 'PUT',
+            data: JSON.stringify(reference)
+        });
+    }
+
+    render() {
+        const itemsHtml = this.references.map(reference => {
+            return `
+<li class="list-group-item d-flex justify-content-between align-items-center" data-id="${reference.id}">
+    <span class="drag-handle fa fa-reorder"></span>
+    <input type="text" value="${reference.originalFilename}" class="form-control js-edit-filename" style="width: auto;">
+
+    <span>
+        <a href="/admin/article/references/${reference.id}/download" class="btn btn-link btn-sm"><span class="fa fa-download" style="vertical-align: middle"></span></a>
+        <button class="js-reference-delete btn btn-link btn-sm"><span class="fa fa-trash"></span></button>
+    </span>
+</li>
+`
+        });
+
+        this.$element.html(itemsHtml.join(''));
+    }
+}
 
 /*
     Copy that name, and, below, add it: function initializeDropzone().
