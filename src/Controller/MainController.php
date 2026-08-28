5c3f020a5d49ed46872bb58bc07613354045fb89
Khalil Raies <khalil.raies@check24.de>
2026-08-28 18:05:04 +0200

    Course 12 - Chap 9: Frontend Item View: Cleanup

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,15 +9,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function homepage(RecipeRepository $recipeRepository): Response
+    public function homepage(): Response
     {
-        $latestRecipes = $recipeRepository->createQueryBuilderOrderedByNewest()
-            ->setMaxResults(4)
-            ->getQuery()
-            ->getResult();
 
         return $this->render('main/homepage.html.twig', [
-            'latestRecipes' => $latestRecipes,
         ]);
     }
 }
